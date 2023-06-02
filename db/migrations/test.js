2e97195feb6815e1b4a4ce98a75d2f4a269bedce
ilyaizr <ilyaizrailyan@gmail.com>
2023-06-03 02:31:12 +0400

    add tests to check simplify geojson

---
+++ db/migrations/test.js
@@ -0,0 +1,23 @@
+const simplify = require('@turf/simplify')
+
+const fs = require('fs');
+const { geojsonExample } = require('./geojson');
+
+// RU-SPE admin polygon
+
+
+var simplified = simplify(geojsonExample, { tolerance: 0.002, highQuality: true, mutate: true })
+
+function writeJSONToFile(data) {
+  const jsonString = JSON.stringify(data);
+
+  fs.writeFile('./output.json', jsonString, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing JSON to file:', err);
+    } else {
+      console.log('JSON data written to file successfully!');
+    }
+  });
+}
+
+writeJSONToFile(simplified)
