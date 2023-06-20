01d5d99277bd5541bbf7046fdee25a7da6183212
ilyaizr <ilyaizrailyan@gmail.com>
2023-06-20 16:00:29 +0400

    patch up cleansing

--- db/cleansing/places.js
+++ db/cleansing/places.js
@@ -8,8 +8,12 @@ async function cleanPlaces(closeConnection = false) {
   try {
     const removeAmountZeroQuery = `DELETE FROM places WHERE amount = 0`;
     const removeXYZeroQuery = `DELETE FROM places WHERE x = 0 AND y = 0`;
+    const removeNoCoordsQuery = `DELETE FROM places WHERE lng = 0.000000 AND lat = 0.000000`;
+    const removeEmptyPolygon = `DELETE FROM places WHERE polygon = NULL`;
     await dbConn.query(removeAmountZeroQuery);
     await dbConn.query(removeXYZeroQuery);
+    await dbConn.query(removeNoCoordsQuery);
+    await dbConn.query(removeEmptyPolygon);
 
     console.log('cleaning places completed ✅');
   } catch (error) {
