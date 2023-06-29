ce1801e73c26ed18b1acc2419cdb6a786fe490ee
ilyaizr <ilyaizrailyan@gmail.com>
2023-06-30 01:52:25 +0400

    add script to search for missing keys from code

--- client/src/rest/localization/compare.js
+++ client/src/rest/localization/compare.js
@@ -1,3 +1,9 @@
+// const fs = require('fs');
+// const path = require('path');
+// const { enLocale } = require('./enLocale.js');
+// const { ruLocale } = require('./ruLocale.js');
+import fs from 'fs'
+import path from 'path'
 import { enLocale } from "./enLocale.js";
 import { ruLocale } from "./ruLocale.js";
 
@@ -26,3 +32,55 @@ function compareLocales() {
 }
 
 compareLocales();
+
+
+const filesPath = path.join(process.cwd(), '../../');
+
+function searchAndCompareKeys() {
+  const missingKeys = [];
+
+  // Traverse files in the 'src' directory
+  traverseDirectory(filesPath, (filePath) => {
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+
+    // Search for occurrences of TEXT.<something>
+    const regex = /TEXT\.([^\s.;'"<>{}(),]+)/g;
+
+    let match;
+    while ((match = regex.exec(fileContents)) !== null) {
+      const key = match[1];
+
+      // Check if the key exists in enLocale
+      if (!(key in enLocale)) {
+        missingKeys.push(`Key '${key}' missing in the en localization`);
+      }
+
+      // Check if the key exists in ruLocale
+      if (!(key in ruLocale)) {
+        missingKeys.push(`Key '${key}' missing in the ru localization`);
+      }
+    }
+  });
+
+  if (!missingKeys.length) {
+    console.log('No particular keys from code are missing! Well done 4 sure ✅✅✅✅');
+  } else {
+    missingKeys.forEach((missingKey) => {
+      console.log(missingKey);
+    });
+  }
+}
+
+function traverseDirectory(directoryPath, callback) {
+  fs.readdirSync(directoryPath).forEach((file) => {
+    const filePath = path.join(directoryPath, file);
+    const stats = fs.statSync(filePath);
+    if (stats.isDirectory()) {
+      traverseDirectory(filePath, callback);
+    } else {
+      callback(filePath);
+    }
+  });
+}
+
+searchAndCompareKeys();
