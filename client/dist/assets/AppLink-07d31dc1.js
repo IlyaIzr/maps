ccf2109f77a2730683acb0cceab783698702ac6e
Ilya <50058726+IlyaIzr@users.noreply.github.com>
2023-07-27 13:00:10 +0000

    Update 52 files

--- client/dist/assets/AppLink-07d31dc1.js
+++
@@ -1,1 +0,0 @@
-import{v as x,j as f,Q as j}from"./index-fe52dfb8.js";function l({to:c,children:u,...t}){const a=x();function e(h){var n;a.push(j(c)),(n=t==null?void 0:t.onClick)==null||n.call(t,h)}return f.jsx("div",{...t,onClick:e,children:u})}export{l as A};
